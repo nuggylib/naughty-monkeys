ef9dabaf1c3bda88d0f833542664585982a99155
Armando Vasquez <ar.xa.vasquez@gmail.com>
2022-05-04 17:01:27 -0500

    make monkey poo a projectile

---
+++ src/main/java/com/nuggylib/naughtymonkeys/client/renderer/entity/ThrownMonkeyPooRenderer.java
@@ -0,0 +1,4 @@
+package com.nuggylib.naughtymonkeys.client.renderer.entity;
+
+public class ThrownMonkeyPooRenderer {
+}
---
+++ src/main/java/com/nuggylib/naughtymonkeys/common/entity/projectile/ThrownMonkeyPoo.java
@@ -0,0 +1,43 @@
+package com.nuggylib.naughtymonkeys.common.entity.projectile;
+
+import com.nuggylib.naughtymonkeys.common.registries.entity.NaughtyMonkeysEntities;
+import com.nuggylib.naughtymonkeys.common.registries.items.NaughtyMonkeysItems;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.HitResult;
+import org.jetbrains.annotations.NotNull;
+
+public class ThrownMonkeyPoo extends ThrowableItemProjectile {
+
+    public ThrownMonkeyPoo(Level p_37399_, LivingEntity p_37400_) {
+        super(NaughtyMonkeysEntities.THROWN_MONKEY_POO.get(), p_37400_, p_37399_);
+    }
+    public ThrownMonkeyPoo(EntityType<ThrownMonkeyPoo> p_37442_, Level p_37443_) {
+        super(p_37442_, p_37443_);
+    }
+
+    @Override
+    protected void onHitEntity(@NotNull EntityHitResult target) {
+        super.onHitEntity(target);
+        target.getEntity().hurt(DamageSource.thrown(this, this.getOwner()), 1.0F);
+    }
+
+    @Override
+    protected void onHit(@NotNull HitResult hitResult) {
+        super.onHit(hitResult);
+        if (!this.level.isClientSide) {
+            this.level.broadcastEntityEvent(this, (byte)3);
+            this.discard();
+        }
+    }
+
+    @Override
+    protected Item getDefaultItem() {
+        return NaughtyMonkeysItems.MONKEY_POO.get();
+    }
+}
