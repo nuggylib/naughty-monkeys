979e9dc7fe11760b649b79e86917b2d234572da3
Armando Vasquez <ar.xa.vasquez@gmail.com>
2022-05-04 00:14:32 -0500

    add banana and make it tempt monkey entity

--- src/main/java/com/nuggylib/naughtymonkeys/common/registries/items/NaughtyMonkeysItems.java
+++ src/main/java/com/nuggylib/naughtymonkeys/common/registries/items/NaughtyMonkeysItems.java
@@ -1,6 +1,7 @@
 package com.nuggylib.naughtymonkeys.common.registries.items;
 
 import com.nuggylib.naughtymonkeys.common.NaughtyMonkeys;
+import com.nuggylib.naughtymonkeys.common.item.ItemBanana;
 import com.nuggylib.naughtymonkeys.common.registries.blocks.NaughtyMonkeysBlocks;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
@@ -20,6 +21,7 @@ public class NaughtyMonkeysItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, NaughtyMonkeys.ID);
 
     public static final RegistryObject<Item> BLOCK_OF_MONKEY_POO_ITEM = ITEMS.register("block_of_monkey_poo", () -> new BlockItem(NaughtyMonkeysBlocks.BLOCK_OF_MONKEY_POO.get(), new Item.Properties().tab(NaughtyMonkeys.TAB_NAUGHTY_MONKEYS)));
+    public static final RegistryObject<Item> BANANA = ITEMS.register("banana", () -> new ItemBanana(new Item.Properties().tab(NaughtyMonkeys.TAB_NAUGHTY_MONKEYS)));
 
     public static Item.Properties defaultBuilder() {
         return new Item.Properties().tab(NaughtyMonkeys.TAB_NAUGHTY_MONKEYS);
