72b6093a7a5c93cba20d6eafdedda9a7b24ceef8
Armando Vasquez <ar.xa.vasquez@gmail.com>
2022-05-06 19:15:05 -0500

    add banana leaves model and texture

--- src/main/java/com/nuggylib/naughtymonkeys/common/registries/blocks/NaughtyMonkeysBlocks.java
+++ src/main/java/com/nuggylib/naughtymonkeys/common/registries/blocks/NaughtyMonkeysBlocks.java
@@ -4,9 +4,14 @@ import com.nuggylib.naughtymonkeys.common.NaughtyMonkeys;
 import com.nuggylib.naughtymonkeys.common.block.BlockOfBanana;
 import com.nuggylib.naughtymonkeys.common.block.BlockOfMonkeyPoo;
 import com.nuggylib.naughtymonkeys.common.block.grower.BananaPlantGrower;
+import com.nuggylib.naughtymonkeys.common.registries.entity.NaughtyMonkeysEntities;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
 import net.minecraftforge.registries.DeferredRegister;
@@ -27,6 +32,7 @@ public class NaughtyMonkeysBlocks {
     public static final RegistryObject<Block> BLOCK_OF_MONKEY_POO = BLOCKS.register("block_of_monkey_poo", () -> new BlockOfMonkeyPoo(BlockBehaviour.Properties.of(Material.DIRT).strength(2.0F, 6.0F).sound(SoundType.SLIME_BLOCK)));
     public static final RegistryObject<Block> BLOCK_OF_BANANA = BLOCKS.register("block_of_banana", () -> new BlockOfBanana(BlockBehaviour.Properties.of(Material.DIRT).strength(2.0F, 6.0F).sound(SoundType.SLIME_BLOCK)));
     public static final RegistryObject<Block> BANANA_STEM = BLOCKS.register("banana_stem", () -> log(MaterialColor.WOOD, MaterialColor.PODZOL));
+    public static final RegistryObject<Block> BANANA_LEAVES = BLOCKS.register("banana_leaves", () ->  leaves(SoundType.GRASS));
     public static final RegistryObject<Block> BANANA_SAPLING = BLOCKS.register("banana_sapling", () -> new SaplingBlock(new BananaPlantGrower(), BlockBehaviour.Properties.of(Material.PLANT).noCollission().randomTicks().instabreak().sound(SoundType.GRASS)));
 
     /**
@@ -40,6 +46,31 @@ public class NaughtyMonkeysBlocks {
         return new RotatedPillarBlock(BlockBehaviour.Properties.of(Material.WOOD, (blockState) -> {
             return blockState.getValue(RotatedPillarBlock.AXIS) == Direction.Axis.Y ? barkColor : coreColor;
         }).strength(2.0F).sound(SoundType.WOOD));
+    }
+
+    /**
+     * Static helper used to determine if the given entity is a monkey.
+     *
+     * This method is used to control what can spawn on the banana leaves
+     *
+     * @return  true if the given entity is a monkey, otherwise false
+     */
+    private static Boolean monkey(BlockState p_50822_, BlockGetter p_50823_, BlockPos p_50824_, EntityType<?> p_50825_) {
+        return p_50825_ == NaughtyMonkeysEntities.MONKEY.get();
+    }
+
+    /**
+     * Static helper method taken from Minecraft's {@link net.minecraft.world.level.block.Blocks} class used to generate leaves blocks for registration
+     */
+    private static LeavesBlock leaves(SoundType soundType) {
+        return new LeavesBlock(BlockBehaviour.Properties.of(Material.LEAVES).strength(0.2F).randomTicks().sound(soundType).noOcclusion().isValidSpawn(NaughtyMonkeysBlocks::monkey).isSuffocating(NaughtyMonkeysBlocks::never).isViewBlocking(NaughtyMonkeysBlocks::never));
+    }
+
+    /**
+     * Static helper method taken from Minecraft's {@link net.minecraft.world.level.block.Blocks} class - always returns false
+     */
+    private static Boolean never(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos) {
+        return (boolean)false;
     }
     
 }
