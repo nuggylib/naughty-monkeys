2db4b1ff85996eb6748e35eef027603903bece9b
armstrongl <armstrong.llarah@gmail.com>
2022-05-07 12:28:56 -0500

    Update ModelBabyMonkey.java

--- src/main/java/com/nuggylib/naughtymonkeys/client/model/entity/ModelBabyMonkey.java
+++ src/main/java/com/nuggylib/naughtymonkeys/client/model/entity/ModelBabyMonkey.java
@@ -7,70 +7,68 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.nuggylib.naughtymonkeys.common.NaughtyMonkeys;
 import com.nuggylib.naughtymonkeys.common.entity.BabyMonkey;
-import com.nuggylib.naughtymonkeys.common.entity.Monkey;
-import net.minecraft.client.model.EntityModel;
-import net.minecraft.client.model.QuadrupedModel;
-import net.minecraft.client.model.SheepModel;
+import net.minecraft.client.model.AgeableListModel;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.*;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
-import net.minecraft.world.entity.Entity;
 
 /**
  * Created using Blockbench
  *
  * @param <T>
  */
-public class ModelBabyMonkey<T extends BabyMonkey> extends EntityModel<T> {
+public class ModelBabyMonkey<T extends BabyMonkey> extends AgeableListModel<T> {
     // This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
     public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation(NaughtyMonkeys.ID, "baby_monkey"), "main");
-    private final ModelPart torso;
+    private final ModelPart body;
     private final ModelPart head;
     private final ModelPart tail;
-    private final ModelPart arm_left;
-    private final ModelPart arm_right;
-    private final ModelPart leg_left;
-    private final ModelPart leg_right;
+    private final ModelPart leftFrontLeg;
+    private final ModelPart rightFrontLeg;
+    private final ModelPart leftHindLeg;
+    private final ModelPart rightHindLeg;
     private float headXRot;
+    private final boolean scaleHead;
+    private final float babyYHeadOffset;
+    private final float babyZHeadOffset;
+    private final float babyHeadScale;
+    private final float babyBodyScale;
+    private final float bodyYOffset;
 
     public ModelBabyMonkey(ModelPart root) {
-        this.torso = root.getChild("torso");
+        this.body = root.getChild("torso");
         this.head = root.getChild("head");
         this.tail = root.getChild("tail");
-        this.arm_left = root.getChild("arm_left");
-        this.arm_right = root.getChild("arm_right");
-        this.leg_left = root.getChild("leg_left");
-        this.leg_right = root.getChild("leg_right");
+        this.leftFrontLeg = root.getChild("arm_left");
+        this.rightFrontLeg = root.getChild("arm_right");
+        this.leftHindLeg = root.getChild("leg_left");
+        this.rightHindLeg = root.getChild("leg_right");
+        this.scaleHead = true;
+        this.babyYHeadOffset = 0;
+        this.babyZHeadOffset = 0;
+        this.babyHeadScale = 2;
+//      The scale of the baby's body is determined by 1 / babyBodyScale
+        this.babyBodyScale = 2;
+        this.bodyYOffset = 0;
     }
 
     public static LayerDefinition createBodyLayer() {
         MeshDefinition meshdefinition = new MeshDefinition();
         PartDefinition partdefinition = meshdefinition.getRoot();
-
         PartDefinition torso = partdefinition.addOrReplaceChild("torso", CubeListBuilder.create().texOffs(0, 11).addBox(-2.0F, -5.75F, -3.0F, 4.0F, 3.0F, 6.0F, new CubeDeformation(0.0F))
                 .texOffs(0, 0).addBox(-0.5F, -5.5F, -3.75F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
-
         PartDefinition head = partdefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 0).addBox(-3.5F, -1.0F, -5.0F, 7.0F, 6.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 16.5F, -3.25F));
-
         PartDefinition tail = partdefinition.addOrReplaceChild("tail", CubeListBuilder.create().texOffs(14, 11).addBox(-1.1926F, 1.5796F, -8.4887F, 1.0F, 1.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.6926F, 17.6704F, 11.4887F));
-
         PartDefinition tail_3_r1 = tail.addOrReplaceChild("tail_3_r1", CubeListBuilder.create().texOffs(17, 17).addBox(-2.5F, -3.734F, -5.1428F, 1.0F, 1.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.3074F, 0.4047F, 1.3183F, 0.6981F, 0.0F, -0.6109F));
-
         PartDefinition tail_3_r2 = tail.addOrReplaceChild("tail_3_r2", CubeListBuilder.create().texOffs(0, 0).addBox(-2.5F, 4.0249F, -7.2373F, 1.0F, -3.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.3074F, 0.4047F, 1.3183F, -0.829F, 0.0F, -0.6109F));
-
         PartDefinition tail_2_r1 = tail.addOrReplaceChild("tail_2_r1", CubeListBuilder.create().texOffs(19, 0).addBox(-2.5F, 1.0249F, -6.2373F, 1.0F, 1.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.3074F, 0.4047F, 1.3183F, -0.829F, 0.0F, -0.6109F));
-
         PartDefinition arm_left = partdefinition.addOrReplaceChild("arm_left", CubeListBuilder.create().texOffs(8, 20).addBox(-0.5F, 1.0F, -0.5F, 1.0F, 6.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(1.75F, 17.0F, -2.5F));
-
         PartDefinition arm_right = partdefinition.addOrReplaceChild("arm_right", CubeListBuilder.create().texOffs(12, 20).addBox(-0.5F, 1.0F, -0.5F, 1.0F, 6.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(-1.75F, 17.0F, -2.5F));
-
         PartDefinition leg_left = partdefinition.addOrReplaceChild("leg_left", CubeListBuilder.create().texOffs(4, 20).addBox(-0.5F, 1.0F, -3.5F, 1.0F, 6.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(1.75F, 17.0F, 5.5F));
-
         PartDefinition leg_right = partdefinition.addOrReplaceChild("leg_right", CubeListBuilder.create().texOffs(0, 20).addBox(-0.5F, 1.0F, -3.5F, 1.0F, 6.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(-1.75F, 17.0F, 5.5F));
-
         return LayerDefinition.create(meshdefinition, 32, 32);
     }
 
@@ -84,20 +82,31 @@ public class ModelBabyMonkey<T extends BabyMonkey> extends EntityModel<T> {
     public void setupAnim(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
         this.head.xRot = headPitch * ((float)Math.PI / 180F);
         this.head.yRot = netHeadYaw * ((float)Math.PI / 180F);
-        this.leg_right.xRot = Mth.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
-        this.leg_left.xRot = Mth.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
-        this.arm_right.xRot = Mth.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
-        this.arm_left.xRot = Mth.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
+        this.rightHindLeg.xRot = Mth.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
+        this.leftHindLeg.xRot = Mth.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
+        this.rightFrontLeg.xRot = Mth.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
+        this.leftFrontLeg.xRot = Mth.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
     }
 
     @Override
     public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
-        torso.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+        body.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
         head.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
         tail.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
-        arm_left.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
-        arm_right.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
-        leg_left.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
-        leg_right.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+        leftFrontLeg.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+        rightFrontLeg.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+        leftHindLeg.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+        rightHindLeg.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+
+    }
+
+    @Override
+    protected Iterable<ModelPart> headParts() {
+        return null;
+    }
+
+    @Override
+    protected Iterable<ModelPart> bodyParts() {
+        return null;
     }
 }
