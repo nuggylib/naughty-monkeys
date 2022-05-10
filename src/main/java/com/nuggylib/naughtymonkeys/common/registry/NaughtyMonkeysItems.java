bfa7b7488fd23ec986aae77a016b9d34172f4f08
Armando Vasquez <ar.xa.vasquez@gmail.com>
2022-05-10 00:42:55 -0500

    WIP - need to fix texture; more to do for crop logic

--- src/main/java/com/nuggylib/naughtymonkeys/common/registry/NaughtyMonkeysItems.java
+++ src/main/java/com/nuggylib/naughtymonkeys/common/registry/NaughtyMonkeysItems.java
@@ -20,11 +20,15 @@ public class NaughtyMonkeysItems {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, NaughtyMonkeys.ID);
 
-    public static final RegistryObject<Item> BLOCK_OF_MONKEY_POO_ITEM = ITEMS.register("block_of_monkey_poo", () -> new BlockItem(NaughtyMonkeysBlocks.BLOCK_OF_MONKEY_POO.get(), new Item.Properties().tab(NaughtyMonkeys.TAB_NAUGHTY_MONKEYS)));
-    public static final RegistryObject<Item> BLOCK_OF_BANANA_ITEM = ITEMS.register("block_of_banana", () -> new BlockItem(NaughtyMonkeysBlocks.BLOCK_OF_BANANA.get(), new Item.Properties().tab(NaughtyMonkeys.TAB_NAUGHTY_MONKEYS)));
-    public static final RegistryObject<Item> BANANA_STEM_ITEM = ITEMS.register("banana_stem", () -> new BlockItem(NaughtyMonkeysBlocks.BANANA_STEM.get(), new Item.Properties().tab(NaughtyMonkeys.TAB_NAUGHTY_MONKEYS)));
-    public static final RegistryObject<Item> BANANA_LEAVES_ITEM = ITEMS.register("banana_leaves", () -> new BlockItem(NaughtyMonkeysBlocks.BANANA_LEAVES.get(), new Item.Properties().tab(NaughtyMonkeys.TAB_NAUGHTY_MONKEYS)));
+    // Blocks
+    public static final RegistryObject<BlockItem> BLOCK_OF_MONKEY_POO_ITEM = ITEMS.register("block_of_monkey_poo", () -> new BlockItem(NaughtyMonkeysBlocks.BLOCK_OF_MONKEY_POO.get(), new Item.Properties().tab(NaughtyMonkeys.TAB_NAUGHTY_MONKEYS)));
+    public static final RegistryObject<BlockItem> BLOCK_OF_BANANA_ITEM = ITEMS.register("block_of_banana", () -> new BlockItem(NaughtyMonkeysBlocks.BLOCK_OF_BANANA.get(), new Item.Properties().tab(NaughtyMonkeys.TAB_NAUGHTY_MONKEYS)));
+    public static final RegistryObject<BlockItem> BANANA_STEM_ITEM = ITEMS.register("banana_stem", () -> new BlockItem(NaughtyMonkeysBlocks.BANANA_STEM.get(), new Item.Properties().tab(NaughtyMonkeys.TAB_NAUGHTY_MONKEYS)));
+    public static final RegistryObject<BlockItem> BANANA_LEAVES_ITEM = ITEMS.register("banana_leaves", () -> new BlockItem(NaughtyMonkeysBlocks.BANANA_LEAVES.get(), new Item.Properties().tab(NaughtyMonkeys.TAB_NAUGHTY_MONKEYS)));
     public static final RegistryObject<BlockItem> BANANA_SAPLING_ITEM = ITEMS.register("banana_sapling", () -> new BlockItem(NaughtyMonkeysBlocks.BANANA_SAPLING.get(), new Item.Properties().tab(NaughtyMonkeys.TAB_NAUGHTY_MONKEYS)));
+    public static final RegistryObject<BlockItem> BANANAS = ITEMS.register("bananas", () -> new BlockItem(NaughtyMonkeysBlocks.BANANAS.get(), new Item.Properties().tab(NaughtyMonkeys.TAB_NAUGHTY_MONKEYS)));
+
+    // Items
     public static final RegistryObject<Item> BANANA = ITEMS.register("banana", () -> new ItemBanana(new Item.Properties().tab(NaughtyMonkeys.TAB_NAUGHTY_MONKEYS)));
     public static final RegistryObject<Item> MONKEY_POO = ITEMS.register("monkey_poo", () -> new ItemMonkeyPoo(new Item.Properties().tab(NaughtyMonkeys.TAB_NAUGHTY_MONKEYS)));
 
