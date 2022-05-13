c6b0a8a51c51c59002a06c120fa3f01b5bc7398e
Armando Vasquez <ar.xa.vasquez@gmail.com>
2022-05-12 22:27:59 -0500

    WIP - need to finish registration for sounds

---
+++ src/main/java/com/nuggylib/naughtymonkeys/common/registry/NaughtyMonkeysSounds.java
@@ -0,0 +1,16 @@
+package com.nuggylib.naughtymonkeys.common.registry;
+
+import com.nuggylib.naughtymonkeys.common.NaughtyMonkeys;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class NaughtyMonkeysSounds {
+
+    public static final DeferredRegister<SoundEvent> SOUNDS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, NaughtyMonkeys.ID);
+
+    public static final RegistryObject<SoundEvent> MONKEY_POO_SPLAY = SOUNDS.register("entity.monkey_poo.hit", () -> new SoundEvent(new ResourceLocation("entity.monkey_poo.hit")));
+
+}
