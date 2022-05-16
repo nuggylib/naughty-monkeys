6a530113f12cb8ffdc36db56cf113b80526c7693
Armando Vasquez <ar.xa.vasquez@gmail.com>
2022-05-15 19:22:07 -0500

    simplify tree logic - no need to use registration

---
+++ docs/forge/Readme.md
@@ -0,0 +1,17 @@
+# Forge Readme
+
+Since the Forge documentation is very lacking and often inaccurate, I feel it necessary to document relevant concepts
+as we fight our way through them.
+
+## Coremodding
+
+Coremods are special types of mods that have the ability to modify every piece of code in vanilla minecraft. The creator
+of Forge, LexManos, has this to say about coremods:
+* 99.99% of mods don't need a coremod
+* 99% of coremods should at least be attempted to be put into Forge
+* 1% of coremods are actually doing something useful or that is wholly specific for their mods (and don't play well
+  within the Forge ecosystem)
+
+> Forge Coremodding resources
+> * https://forums.minecraftforge.net/topic/62937-what-are-coremods/
+> * https://forums.minecraftforge.net/topic/62870-what-is-a-coremod-and-what-are-it-advantages-and-disadvantages/
--- src/main/java/com/nuggylib/naughtymonkeys/common/registry/NaughtyMonkeysFeatures.java
+++ src/main/java/com/nuggylib/naughtymonkeys/common/registry/NaughtyMonkeysFeatures.java
@@ -1,48 +1,31 @@
 package com.nuggylib.naughtymonkeys.common.registry;
 
-import com.google.common.collect.ImmutableList;
 import com.nuggylib.naughtymonkeys.common.NaughtyMonkeys;
-import com.nuggylib.naughtymonkeys.common.world.level.levelgen.feature.BananaPlantFoliagePlacer;
-import com.nuggylib.naughtymonkeys.common.world.level.levelgen.feature.treedecorator.BananasDecorator;
-import net.minecraft.core.Registry;
-import net.minecraft.data.BuiltinRegistries;
-import net.minecraft.util.valueproviders.ConstantInt;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
-import net.minecraft.world.level.levelgen.feature.Feature;
-import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
-import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
-import net.minecraft.world.level.levelgen.feature.featuresize.TwoLayersFeatureSize;
-import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
-import net.minecraft.world.level.levelgen.feature.trunkplacers.StraightTrunkPlacer;
+import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.level.biome.BiomeSpecialEffects;
+import net.minecraft.world.level.biome.MobSpawnSettings;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+
+import java.util.List;
 
 @Mod.EventBusSubscriber(modid = NaughtyMonkeys.ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class NaughtyMonkeysFeatures {
 
-    public static ConfiguredFeature<TreeConfiguration, ?> BANANA_PLANT;
+    @SubscribeEvent(priority = EventPriority.HIGH)
+    public void onBiomeLoadingEvent(BiomeLoadingEvent event) {
+        List<MobSpawnSettings.SpawnerData> spawns = event.getSpawns().getSpawner(MobCategory.CREATURE);
 
-    public static void init() {
-        BANANA_PLANT = register("banana_plant", Feature.TREE.configured(createBananaPlant().decorators(ImmutableList.of(new BananasDecorator(1.0F))).ignoreVines().build()));
-    }
+        // Remove "default" spawn logic inherited for Monkeys
+        spawns.removeIf(e -> e.type == NaughtyMonkeysEntities.MONKEY.get());
+        // Make monkeys spawn more
+        spawns.add(new MobSpawnSettings.SpawnerData(NaughtyMonkeysEntities.MONKEY.get(), 10000, 1, 4));
 
-    private static TreeConfiguration.TreeConfigurationBuilder createStraightBlobTree(Block logBlock, Block leavesBlock, int baseHeight, int heightRandA, int heightRandB, int p_195152_) {
-        return new TreeConfiguration.TreeConfigurationBuilder(BlockStateProvider.simple(logBlock), new StraightTrunkPlacer(baseHeight, heightRandA, heightRandB), BlockStateProvider.simple(leavesBlock), new BananaPlantFoliagePlacer(ConstantInt.of(p_195152_), ConstantInt.of(0), 3), new TwoLayersFeatureSize(1, 0, 1));
-    }
+        BiomeSpecialEffects effects = event.getEffects();
 
-    private static TreeConfiguration.TreeConfigurationBuilder createBananaPlant() {
-        return createStraightBlobTree(NaughtyMonkeysBlocks.BANANA_STEM.get(), NaughtyMonkeysBlocks.BANANA_LEAVES.get(), 4, 2, 0, 2).ignoreVines();
-    }
 
-    private static <FC extends FeatureConfiguration> ConfiguredFeature<FC, ?> register(String key, ConfiguredFeature<FC, ?> configuredFeature) {
-        return Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, key, configuredFeature);
-    }
-
-    @SubscribeEvent
-    public static void onCommonSetup(final FMLCommonSetupEvent event) {
-        event.enqueueWork(NaughtyMonkeysFeatures::init);
     }
 
 }
