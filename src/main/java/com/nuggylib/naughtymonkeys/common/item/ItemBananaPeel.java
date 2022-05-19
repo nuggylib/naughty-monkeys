9eb2df0774b084a03fcdd7781ce5eebbe19c635e
Armando Vasquez <ar.xa.vasquez@gmail.com>
2022-05-19 18:30:33 -0500

    add banana peel

--- src/main/java/com/nuggylib/naughtymonkeys/common/item/ItemBananaPeel.java
+++ src/main/java/com/nuggylib/naughtymonkeys/common/item/ItemBananaPeel.java
@@ -2,8 +2,8 @@ package com.nuggylib.naughtymonkeys.common.item;
 
 import net.minecraft.world.item.Item;
 
-public class ItemBananaBunch extends Item {
-    public ItemBananaBunch(Properties properties) {
+public class ItemBananaPeel extends Item {
+    public ItemBananaPeel(Properties properties) {
         super(properties);
     }
 }
